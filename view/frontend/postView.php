0ca47216e1e8a452fa759f138c1a3ed8540193db
Vincent <vincetonin2007@hotmail.fr>
2018-01-24 16:29:39 +0100

    Mise à jour de la vue post pour afficher les commentaires sur la page

--- view/frontend/postView.php
+++ view/frontend/postView.php
@@ -7,12 +7,24 @@ ob_start();
 ?>
 	<div class="ticket">
 			<div class="contenaire_title_date">
-				<p><?= htmlspecialchars($data["title"]); ?> <?= $data["creation_date_fr"]; ?></p>
+				<p><?= htmlspecialchars($data["title"]); ?> <?= htmlspecialchars($data["creation_date_fr"]); ?></p>
 			</div>
 	</div>
 <a href="index.php?action=homepage">Retour à la page d'accueil</a>
 </div>
+
+<div id="contenaire-comments">
+	<?php 
+		while($datas = $comms->fetch())
+		{
+	?>
+		<div id="contenaire-comments">
+			<p><?php echo htmlspecialchars($datas["creation_date_fr"]) . "<br>" . htmlspecialchars($datas["content"]); ?></p>
+			<a href="index.php?action=reportcomment&id_post=<?= $_GET['id_post']; ?>&id=<?= $datas['id']; ?>" ><i class="fa fa-exclamation" aria-hidden="true"></i></a>
+		</div>
+</div>
 <?php
+}
 $content = ob_get_clean();
 require("view/frontend/template.php");
 ?>
