3c01810630ae30bc1368966369d4c196b60a1eac
Vincent <vincetonin2007@hotmail.fr>
2018-01-24 15:43:59 +0100

    ajout d'une methode 'getPost' recuperant uniquement 1 seul post grace à une id passez en parametre dans le lien de chaque post de la homepage

--- model/PostManager.php
+++ model/PostManager.php
@@ -8,4 +8,13 @@ class PostManager extends Manager{
 		$posts = $db->query("SELECT id, author, title, content, DATE_FORMAT(creation_date, \" Le %d/%m/%Y à %Hh%imin%ss\") AS creation_date_fr FROM tickets ORDER BY id DESC");
 		return $posts;
 	}
+	public function getPost()
+	{
+		$db = $this->db();
+		$post = $db->prepare("SELECT id, author, title, content, DATE_FORMAT(creation_date, \" Le %d/%m/%Y à %Hh%imin%ss\") AS creation_date_fr FROM tickets WHERE id = :id");
+		$post->execute(array(
+			"id" => $_GET["id_post"]
+		));
+		return $post;
+	}
 }
