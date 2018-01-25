ea8d638b6ca5dbc4b3856fda249cadaee509d650
Vincent <vincetonin2007@hotmail.fr>
2018-01-25 17:02:09 +0100

    Ajout de vérifications sur les signalements (pour eviter qu'un meme utilisateur signale plusieur fois le meme commentaire' à l'aide des cookies

--- controller/controller.php
+++ controller/controller.php
@@ -24,8 +24,33 @@ class Controller{
 	}
 	public static function reportComment()
 	{
-		$commentManager = new CommentManager();
-		$reportedComment = $commentManager->reportComment();
-		header("Location: index.php?action=postpage&id_post=" .$_GET["id_post"]);	
+		if(isset($_COOKIE["reportedCommentList"]))
+		{
+			$cookieUnserialize = unserialize($_COOKIE["reportedCommentList"]);
+			foreach($cookieUnserialize as $reportid)
+			{
+				if($reportid === $_GET["id"])
+				{
+					$_SESSION["alreadyReported"] = true;
+					header("Location: index.php?action=postpage&id_post=" .$_GET["id_post"]);
+					throw new Exception("Commentaire déjà signalé");
+				}
+			}
+			array_push($cookieUnserialize, $_GET["id"]);
+			$cookieSerialize = serialize($cookieUnserialize);
+			setcookie("reportedCommentList", $cookieSerialize, time() + (31556926)); //31556926 = total seconds in one year
+			$commentManager = new CommentManager();
+			$reportedComment = $commentManager->reportComment();
+			header("Location: index.php?action=postpage&id_post=" .$_GET["id_post"]);					
+		}
+		else
+		{
+			$tableReportComment = array($_GET["id"]);
+			$tableReportCommentSerialize = serialize($tableReportComment);
+			setcookie("reportedCommentList", $tableReportCommentSerialize, time() + (31556926));
+			$commentManager = new CommentManager();
+			$reportedComment = $commentManager->reportComment();
+			header("Location: index.php?action=postpage&id_post=" .$_GET["id_post"]);
+		}
 	}
 }
