ba89a2967700922d97a3b13210033da463068324
Vincent <vincetonin2007@hotmail.fr>
2018-01-26 17:14:16 +0100

    Modification d'une balise <p> en balise <h1>, ajout de htmlspecialchars sur les commentaires et d'un nl2br() sur l'affiche du contenu du billet

--- view/frontend/postView.php
+++ view/frontend/postView.php
@@ -8,8 +8,8 @@ ob_start();
 	$data = $post->fetch();
 ?>
 	<div class="ticket">
-		<span class="title"><p><?= htmlspecialchars($data["title"]); ?></p></span>
-		<span class="text"><p><?= htmlspecialchars($data["content"]); ?></p></span>
+		<span class="title"><h1><?= htmlspecialchars($data["title"]); ?></h1></span>
+		<span class="text"><p><?= nl2br(htmlspecialchars($data["content"])); ?></p></span>
 		<span class="author"><p><?= htmlspecialchars($data["author"]); ?></p></span>
 		<span class ="date"><p><?= htmlspecialchars($data["creation_date_fr"]); ?></p></span>
 	</div>
@@ -22,7 +22,7 @@ while($datas = $comms->fetch())
 {
 ?>
 	<div id="contenaire-comments">
-		<p><?php echo $datas["creation_date_fr"] . "<br>" . $datas["content"]; ?></p>
+		<p><?php echo htmlspecialchars($datas["creation_date_fr"]) . "<br>" . htmlspecialchars($datas["content"]); ?></p>
 		<a href="index.php?action=reportcomment&id_post=<?= $_GET['id_post']; ?>&id=<?= $datas['id']; ?>" ><i class="fa fa-exclamation" aria-hidden="true"></i></a>
 	</div>
 </div>
