75fe755902036687b969790e369bd30e16415431
Vincent <vincetonin2007@hotmail.fr>
2018-01-26 20:47:01 +0100

    Ajout d'une balise viewport

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -4,6 +4,7 @@
 			<meta charset="utf-8" />
 			<link rel="stylesheet" href="public/<?= $cssFile; ?>" />
 			<link rel="stylesheet" href="public/font-awesome-4.7.0/css/font-awesome.min">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 			<title> <?= $title ?></title>
 		</head>
 
