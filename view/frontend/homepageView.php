99d3f758a221256f9ea94fea4a35b9711a9f9485
Vincent <vincetonin2007@hotmail.fr>
2018-01-26 15:57:19 +0100

    Ajout d'une div contenant un resumé du blog

--- view/frontend/homepageView.php
+++ view/frontend/homepageView.php
@@ -4,6 +4,14 @@
 ?>
 
 <div id="img-background"></div>
+<div id="main-summarize">
+	<h1>Bienvenue sur mon blog !</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus fermentum nibh at tempor gravida. Praesent tempor arcu dui. Curabitur vitae vehicula nunc, non vulputate ex. Suspendisse potenti. Morbi fringilla ligula non massa fringilla cursus. Nullam condimentum lectus et quam commodo, in pellentesque libero imperdiet. Maecenas vitae arcu et urna faucibus pretium at id dui.</p>
+
+	<p>Suspendisse viverra ipsum sit amet nisl venenatis, eu mollis mi bibendum. Nulla a diam vitae nibh posuere lacinia id a neque. Curabitur hendrerit ex iaculis, rutrum erat quis, dignissim dui. Ut bibendum interdum leo, at consectetur dui egestas nec. Mauris ac varius diam. Integer commodo porttitor congue. Vivamus placerat quam quis quam tristique tempus. Nunc sit amet egestas arcu. Donec ac nisl ac nulla bibendum feugiat. Quisque odio nulla, suscipit ut ipsum eu, congue cursus turpis.</p>
+
+	<p>Vestibulum sit amet nunc aliquam, iaculis justo at, euismod odio. Maecenas in leo sed enim varius viverra. Quisque sit amet ex ipsum. Etiam tempor neque eu risus euismod egestas. Vivamus tempor, metus id dignissim gravida, lacus metus eleifend erat, ac cursus justo orci auctor ex. Nullam maximus.</p>	
+</div>
 <div id="contenaire_ticket">
 <?php
 	while($data = $posts->fetch())
