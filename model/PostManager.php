d107deef16d10288a61dcca5c70e4239e8c02528
Vincent <vincetonin2007@hotmail.fr>
2018-01-27 20:21:03 +0100

    Ajout de parametres dans la methode getPosts (parametre envoye depuis le controller) et concatenation de variable dans la requete SQL permettant d'afficher que 5posts/page

--- model/PostManager.php
+++ model/PostManager.php
@@ -2,10 +2,10 @@
 require_once("model/Manager.php");
 class PostManager extends Manager{
 
-	public function getPosts()
+	public function getPosts($start, $postPerPage)
 	{
 		$db = $this->db();
-		$posts = $db->query("SELECT id, author, title, content, DATE_FORMAT(creation_date, \" Le %d/%m/%Y à %Hh%imin%ss\") AS creation_date_fr FROM tickets ORDER BY id DESC");
+		$posts = $db->query("SELECT id, author, title, content, DATE_FORMAT(creation_date, \" Le %d/%m/%Y à %Hh%imin%ss\") AS creation_date_fr FROM tickets ORDER BY id DESC LIMIT " .$start . ", " . $postPerPage);
 		return $posts;
 	}
 	public function getPost()
