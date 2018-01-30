9dbe9d74e71f893d670562dd70702c75cc7b99b1
Vincent <vincetonin2007@hotmail.fr>
2018-01-30 15:12:07 +0100

    Ajout d'un controller pour gérer les appelles de la homepage

---
+++ controller/controllerHomepage.php
@@ -0,0 +1,16 @@
+<?php
+require_once("model/PostManager.php");
+require_once("model/commentManager.php");
+class ControllerHomePage{
+	public static function homepage()
+	{
+		$postPerPage = 3;
+		$postManager = new PostManager();
+		$totalPost = $postManager->getTotalRow();
+		$totalPage = ceil($totalPost/$postPerPage);
+		$currentPage = $_GET["page"];
+		$start = ($currentPage -1)*$postPerPage;
+		$posts = $postManager->getPosts($start, $postPerPage);
+		require("view/frontend/homepageView.php");
+	}
+}
