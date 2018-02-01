f1314a0a860c993fca2df70a9b8c62a22169a064
Vincent <vincetonin2007@hotmail.fr>
2018-02-01 10:39:42 +0100

    Création d'un fichier JS permettant de modifier le contenu de l apage d'administration dynamiquement

---
+++ public/adaptativeContent.js
@@ -0,0 +1,10 @@
+var ticketsButton = document.querySelector("#tickets-button");
+var commentButton = document.querySelector("#comments-button");
+
+ticketsButton.addEventListener("click", function(){
+	console.log("Ticket !");
+})
+
+commentButton.addEventListener("click", function(){
+	console.log("Comments !");
+})
