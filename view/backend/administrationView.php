1804dc2acc70744fe7f3e8bc7f30ab63f9ee7aaf
Vincent <vincetonin2007@hotmail.fr>
2018-02-01 14:30:13 +0100

    Ajout des titres des articles dans la page (sert de test pour le moment)

--- view/backend/administrationView.php
+++ view/backend/administrationView.php
@@ -4,12 +4,26 @@ $cssFile = "styleAdministration.css";
 ob_start();
 ?>
 <div id="page">
-	<div id="main-content-div"></div>
+	<div id="main-content-div">
+		<div id="tickets-list">
+			<ul>
+				<?php 
+				while($data = $posts->fetch())
+				{
+				?>
+				<li><?= $data["title"]; ?></li>
+				<?php 
+				}
+				?>
+		</div>
+			
+	</div>
 		<div id="menu-bar">
 			<ul>
 				<li>Retourner sur la page d'accueil du blog</li>
 				<li id="tickets-button">Mes tickets</li>
 				<li id="comments-button">Gérer les commentaires</li>
+			</ul>
 		</div>
 </div>
 <script src="public/adaptativeContent.js"></script>
