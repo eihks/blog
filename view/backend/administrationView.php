855e9cb4bd3fb9d25b8bffaff5c79b92f70e3cbb
Vincent <vincetonin2007@hotmail.fr>
2018-02-01 10:17:56 +0100

    Correction d'une erreur dans le chemin du fichier css

--- view/backend/administrationView.php
+++ view/backend/administrationView.php
@@ -1,9 +1,13 @@
 <?php 
 $title = "Administration";
-$cssFile = "public/styleAdministration.css";
+$cssFile = "styleAdministration.css";
 ob_start();
 ?>
 <h1>Administration</h1>
+<div id="main-content-div"></div>
+<nav>
+	<div id="menu-bar"></div>
+</nav>
 <?php
 $content = ob_get_clean();
 require("view/frontend/template.php");
