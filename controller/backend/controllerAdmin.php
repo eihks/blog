7f44220216ec0a8bd6ea6907a840fa940d99054a
Vincent <vincetonin2007@hotmail.fr>
2018-02-01 19:39:54 +0100

    Ajout d'une variable de session permettant de savoir si l'administrateur s'est déjà connecter

--- controller/backend/controllerAdmin.php
+++ controller/backend/controllerAdmin.php
@@ -5,16 +5,27 @@ class controllerAdmin{
 	{
 		$postManager = new PostManager();
 		$posts = $postManager->getPosts();
-		$passwordHash = password_hash("test", PASSWORD_DEFAULT);
-		if(isset($_POST["password"]) AND password_verify($_POST["password"], $passwordHash))
+		if(isset($_SESSION["isLog"]) AND $_SESSION["isLog"] == false)
 		{
 			require("view/backend/administrationView.php");
 		}
 		else
 		{
-			echo "pas ok";
-			echo $passwordHash;
-			require("view/backend/loginView.php");
+			$_SESSION["isLog"] = false;
+			$postManager = new PostManager();
+			$posts = $postManager->getPosts();
+			$passwordHash = password_hash("test", PASSWORD_DEFAULT);
+			if(isset($_POST["password"]) AND password_verify($_POST["password"], $passwordHash))
+			{
+				$_SESSION["isLog"] = true;
+				require("view/backend/administrationView.php");
+			}
+			else
+			{
+				echo "pas ok";
+				echo $passwordHash;
+				require("view/backend/loginView.php");
+			}
 		}
 	}
 
