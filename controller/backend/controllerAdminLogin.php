acff9be80374f19a956fcff28b2f3ed5c575c5bb
Vincent <vincetonin2007@hotmail.fr>
2018-02-01 10:04:01 +0100

    Correction d'une faute de frappe sur la fonction password_verify

--- controller/backend/controllerAdminLogin.php
+++ controller/backend/controllerAdminLogin.php
@@ -3,7 +3,7 @@ class controllerAdminLogin{
 	public static function login()
 	{
 		$passwordHash = password_hash("test", PASSWORD_DEFAULT);
-		if(isset($_POST["password"]) AND password_verifiy($_POST["password"], $passwordHash))
+		if(isset($_POST["password"]) AND password_verify($_POST["password"], $passwordHash))
 		{
 			echo "ok";
 			require("view/backend/loginView.php");
