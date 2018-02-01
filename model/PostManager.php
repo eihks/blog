9faa53b414fa6b059e2a4719ef67168b9bd56378
Vincent <vincetonin2007@hotmail.fr>
2018-02-01 14:21:56 +0100

    Renommage de la methode getPosts en getPostsForHomepage, et ajout d'une nouvelle methode getPosts

--- model/PostManager.php
+++ model/PostManager.php
@@ -2,7 +2,7 @@
 require_once("model/Manager.php");
 class PostManager extends Manager{
 
-	public function getPosts($start, $postPerPage)
+	public function getPostsForHomepage($start, $postPerPage)
 	{
 		$db = $this->db();
 		$posts = $db->query("SELECT id, author, title, content, DATE_FORMAT(creation_date, \" Le %d/%m/%Y à %Hh%imin%ss\") AS creation_date_fr FROM tickets ORDER BY id DESC LIMIT " .$start . ", " . $postPerPage);
@@ -24,5 +24,12 @@ class PostManager extends Manager{
 		$totalRows = $db->query("SELECT id FROM tickets");
 		$totalRows = $totalRows->rowCount();
 		return $totalRows;
+	}
+
+	public function getPosts()
+	{
+		$db $this->db();
+		$posts = db->query("SELECT id, author, title, content, DATE_FORMAT(creation_date, \" Le %d/%m/%Y à %Hh%imin%ss\") AS creation_date_fr FROM tickets ORDER BY id");
+		return $posts:
 	}
 }
