4e2705c4cdfcabf155a40818e314fe4910764c9b
Vincent <vincetonin2007@hotmail.fr>
2018-02-01 14:29:23 +0100

    Ajout d'une instance de la class PostManager et appelle de la methode getPosts

--- controller/backend/controllerAdminLogin.php
+++ controller/backend/controllerAdminLogin.php
@@ -1,7 +1,10 @@
 <?php
+require_once("model/PostManager.php");
 class controllerAdminLogin{
 	public static function login()
 	{
+		$postManager = new PostManager();
+		$posts = $postManager->getPosts();
 		$passwordHash = password_hash("test", PASSWORD_DEFAULT);
 		if(isset($_POST["password"]) AND password_verify($_POST["password"], $passwordHash))
 		{
