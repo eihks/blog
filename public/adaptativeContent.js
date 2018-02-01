27de269a4ec5ffd8ba5bb259145f338692c0f2a9
Vincent <vincetonin2007@hotmail.fr>
2018-02-01 18:10:54 +0100

    Ajout de variable pour récupérer les différents éléments html et ajout de listener pour display none/block lors d'un click sur un li dans le menu de la page

--- public/adaptativeContent.js
+++ public/adaptativeContent.js
@@ -1,10 +1,24 @@
 var ticketsButton = document.querySelector("#tickets-button");
 var commentButton = document.querySelector("#comments-button");
+var divForPosts = document.querySelector("#tickets-list");
+var divEditPost = document.querySelector("#edit-post");
 
 ticketsButton.addEventListener("click", function(){
-	console.log("Ticket !");
+	try{
+		divEditPost.style.display = "none";
+	}
+	catch(e){
+		console.log(e + " : La div n'a pas était chargé");
+	}
+	divForPosts.style.display = "block";
 })
 
 commentButton.addEventListener("click", function(){
-	console.log("Comments !");
+	try{
+		divEditPost.style.display = "none";
+	}
+	catch(e){
+		console.log(e + " : La div n'a pas était chargé");
+	}
+	divForPosts.style.display = "none";
 })
