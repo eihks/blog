2f676370882659834bc1145c2b0b6b542bd51c07
Vincent <vincetonin2007@hotmail.fr>
2018-02-02 11:50:22 +0100

    Ajout d'une methode postUpdate qui se déroule de cette manière -> verifie si l'admin est bien connecter -> appelle le model PostManager -> appelle la methode postUpdate en passant en parametre les valeur  envoyé depuis la vue d'administration et d'une id de post dans l'url et redirige sur la page d'administration

--- controller/backend/controllerAdmin.php
+++ controller/backend/controllerAdmin.php
@@ -3,20 +3,29 @@ require_once("model/PostManager.php");
 class controllerAdmin{
 	public static function login()
 	{
-		$_SESSION["isLog"] = false;
-		$passwordHash = password_hash("test", PASSWORD_DEFAULT);
-		if(isset($_POST["password"]) AND password_verify($_POST["password"], $passwordHash))
+		if(isset($_SESSION["isLog"]) AND $_SESSION["isLog"] == true)
 		{
-			$_SESSION["isLog"] = true;
 			$postManager = new PostManager();
 			$posts = $postManager->getPosts();
 			require("view/backend/administrationView.php");
 		}
 		else
 		{
-			echo "pas ok";
-			echo $passwordHash;
-			require("view/backend/loginView.php");
+			$_SESSION["isLog"] = false;
+			$passwordHash = password_hash("test", PASSWORD_DEFAULT);
+			if(isset($_POST["password"]) AND password_verify($_POST["password"], $passwordHash))
+			{
+				$_SESSION["isLog"] = true;
+				$postManager = new PostManager();
+				$posts = $postManager->getPosts();
+				require("view/backend/administrationView.php");
+			}
+			else
+			{
+				echo "pas ok";
+				echo $passwordHash;
+				require("view/backend/loginView.php");
+			}
 		}
 	}
 
@@ -33,6 +42,20 @@ class controllerAdmin{
 		else
 		{
 			require("view/backend/loginView.php");
+		}
+	}
+
+	public static function updatePost()
+	{
+		if(isset($_SESSION["isLog"]) AND $_SESSION["isLog"] == true)
+		{
+			$postManager = new PostManager();
+			$post = $postManager->updatePost($_POST["title"], $_POST["content"], $_GET["id_post"]);
+			header("Location: index.php?action=administration");
+		}
+		else
+		{
+			throw new Exception("Vous n'étes pas connecter ! ");
 		}
 	}
 }
