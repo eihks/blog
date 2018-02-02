670ccdf44a821cea94ce4fd506cd812115e9845e
Vincent <vincetonin2007@hotmail.fr>
2018-02-02 11:00:49 +0100

    Ajout de verification supplémentaire pour pouvoir accéder à l'espace d'administration (à l'aide d'une variable de session isLog). Cela permet également de corriger une faille qui était présente sur le blog qui permettait d'accéder à l'edition des tickets sans passer par la page Login si on taper directement l'url avec les bonnes informations

--- controller/backend/controllerAdmin.php
+++ controller/backend/controllerAdmin.php
@@ -3,38 +3,36 @@ require_once("model/PostManager.php");
 class controllerAdmin{
 	public static function login()
 	{
-		$postManager = new PostManager();
-		$posts = $postManager->getPosts();
-		if(isset($_SESSION["isLog"]) AND $_SESSION["isLog"] == false)
+		$_SESSION["isLog"] = false;
+		$passwordHash = password_hash("test", PASSWORD_DEFAULT);
+		if(isset($_POST["password"]) AND password_verify($_POST["password"], $passwordHash))
 		{
+			$_SESSION["isLog"] = true;
+			$postManager = new PostManager();
+			$posts = $postManager->getPosts();
 			require("view/backend/administrationView.php");
 		}
 		else
 		{
-			$_SESSION["isLog"] = false;
-			$postManager = new PostManager();
-			$posts = $postManager->getPosts();
-			$passwordHash = password_hash("test", PASSWORD_DEFAULT);
-			if(isset($_POST["password"]) AND password_verify($_POST["password"], $passwordHash))
-			{
-				$_SESSION["isLog"] = true;
-				require("view/backend/administrationView.php");
-			}
-			else
-			{
-				echo "pas ok";
-				echo $passwordHash;
-				require("view/backend/loginView.php");
-			}
+			echo "pas ok";
+			echo $passwordHash;
+			require("view/backend/loginView.php");
 		}
 	}
 
 	public static function editPost()
 	{
-		$postManager = new PostManager();
-		$post = $postManager->getPost();
-		$postManager = new PostManager();
-		$posts = $postManager->getPosts();
-		require("view/backend/administrationView.php");
+		if(isset($_SESSION["isLog"]) AND $_SESSION["isLog"] == true)
+		{
+			$postManager = new PostManager();
+			$post = $postManager->getPost();
+			$postManager = new PostManager();
+			$posts = $postManager->getPosts();
+			require("view/backend/administrationView.php");
+		}
+		else
+		{
+			require("view/backend/loginView.php");
+		}
 	}
 }
