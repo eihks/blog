05cb1ca0dc8ac8463925752afa99b5439a16c221
Vincent <vincetonin2007@hotmail.fr>
2018-02-06 10:57:06 +0100

    Ajout d'une balise link pour fontawesome

--- view/backend/template.php
+++ view/backend/template.php
@@ -3,6 +3,7 @@
 		<head>
 			<meta charset="utf-8" />
 			<link rel="stylesheet" href="public/styleAdministration.css" />
+			<link rel="stylesheet" href="public/fontawesome-free-5.0.6/web-fonts-with-css/css/fontawesome-all">
 			<title><?= $title; ?></title>
 		</head>
 
