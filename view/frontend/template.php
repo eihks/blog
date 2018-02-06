3768dbf9fe2fed04b7e82ec10bb6d2cdc7036e5f
Vincent <vincetonin2007@hotmail.fr>
2018-02-06 10:57:41 +0100

    Mise à jour du fontawesome utiliser vers la dernière version (5.0.6)

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -3,7 +3,7 @@
 		<head>
 			<meta charset="utf-8" />
 			<link rel="stylesheet" href="public/<?= $cssFile; ?>" />
-			<link rel="stylesheet" href="public/font-awesome-4.7.0/css/font-awesome.min">
+			<link rel="stylesheet" href="public/fontawesome-free-5.0.6/web-fonts-with-css/css/fontawesome-all">
 			<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 			<title> <?= $title ?></title>
 		</head>
