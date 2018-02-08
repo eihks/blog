62ea1eb88f38468bc899024d9799bdde5bf37d65
Vincent <vincetonin2007@hotmail.fr>
2018-02-08 17:14:25 +0100

    Ajotu de 2 liens, le premier servant à éditer un commentaire, le deuxieme servant à confirmer un commentaire

--- view/backend/seeCommentView.php
+++ view/backend/seeCommentView.php
@@ -7,6 +7,8 @@ $data = $comment->fetch();
 <div id="comment-contenair">
 	<div id="comment-administration-contenair">
 		<a href="index.php?action=administration&want=deleteComment&comment_id=<?= $data['id']; ?>" title="Supprimer le commentaire" id="delete-comment-btn"><i class="fas fa-trash-alt delete-btn"></i></a>
+		<a href="index.php?action=administration&want=editComment&comment_id=<?= $data['id']; ?>" title="Editer le commentaire" id="edit-comment-btn"><i class="fas fa-edit"></i></a>
+		<a href="index.php?action=administration&want=confirmComment&comment_id<?= $data['id']; ?>" title="Confirmer le commentaire" id="confirm-comment-btn"><i class="fas fa-check-circle"></i></a>
 	</div>
 	<div id="comment-date-contenair">
 		<p><?= $data["creation_date_fr"]; ?></p>
