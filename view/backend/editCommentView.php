0fbf500e62093fe3d650b0f55485c12198db0178
Vincent <vincetonin2007@hotmail.fr>
2018-02-08 17:23:10 +0100

    Création d'une vue permettant la modification d'un commentaire à l'aide d'un formulaire

---
+++ view/backend/editCommentView.php
@@ -0,0 +1,18 @@
+<?php
+$title ="Administration";
+ob_start();
+$data = $comment->fetch();
+?>
+
+<div id="edit-comment">
+	<form action="index.php?action=administration&want=editComment&id_post= <?= $data['id']; ?>" method="POST">
+		<textarea id="textarea-edit-comment" name="content"><?= $data["content"]; ?></textarea>
+		<button type="submit" id="btn-update-comment">Mettre à jour</button>
+		<button type="button" id="btn-stop-update">Annuler</button>
+	</form>
+</div>
+
+<?php
+$content = ob_get_clean();
+require("view/backend/template.php");
+?>
