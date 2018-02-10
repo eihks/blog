dacbfd4c29fc463958025023737f02987af4071c
Vincent <vincetonin2007@hotmail.fr>
2018-02-10 21:28:39 +0100

    Changement de l'id btn-update-ticket en une class btn-update

--- view/backend/editCommentView.php
+++ view/backend/editCommentView.php
@@ -7,7 +7,7 @@ $data = $comment->fetch();
 <div id="edit-comment">
 	<form action="index.php?action=administration&want=editComment&comment_id= <?= $data['id']; ?>" method="POST">
 		<textarea id="textarea-edit-comment" class="mceEditor" name="content"><?= $data["content"]; ?></textarea>
-		<button type="submit" id="btn-update-comment">Mettre à jour</button>
+		<button type="submit" class="btn-update">Mettre à jour</button>
 		<a href="index.php?action=administration&want=seeComments"><button type="button" id="btn-stop-update">Annuler</button></a>
 	</form>
 </div>
