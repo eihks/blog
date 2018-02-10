3e31b98f775a6a3e0ede9ad1223d779da0cdac65
Vincent <vincetonin2007@hotmail.fr>
2018-02-10 16:56:26 +0100

    Ajout d'un lien au bouton 'annuler' pour retourner à la liste des commentaires

--- view/backend/editCommentView.php
+++ view/backend/editCommentView.php
@@ -6,9 +6,9 @@ $data = $comment->fetch();
 
 <div id="edit-comment">
 	<form action="index.php?action=administration&want=editComment&comment_id= <?= $data['id']; ?>" method="POST">
-		<textarea id="textarea-edit-comment" name="content"><?= $data["content"]; ?></textarea>
+		<textarea id="textarea-edit-comment" class="admin-editor" name="content"><?= $data["content"]; ?></textarea>
 		<button type="submit" id="btn-update-comment">Mettre à jour</button>
-		<button type="button" id="btn-stop-update">Annuler</button>
+		<a href="index.php?action=administration&want=seeComments"><button type="button" id="btn-stop-update">Annuler</button></a>
 	</form>
 </div>
 
