125becbef46f64c3af5d840e4e61736a409dd387
Vincent <vincetonin2007@hotmail.fr>
2018-02-10 21:07:12 +0100

    Ajout d'une id au paragraphe affichant le contenu d'un commentaire, suppression de la div contenant les boutons d'administration, ces derniers sont maintenant présent dans la div d'affichage de la date d'ajout du commentaire

--- view/backend/seeCommentView.php
+++ view/backend/seeCommentView.php
@@ -5,17 +5,13 @@ $data = $comment->fetch();
 ?>
 
 <div id="comment-contenair">
-	<div id="comment-administration-contenair">
+	<div id="comment-date-contenair">
 		<a href="index.php?action=administration&want=deleteComment&comment_id=<?= $data['id']; ?>" title="Supprimer le commentaire" id="delete-comment-btn"><i class="fas fa-trash-alt delete-btn"></i></a>
 		<a href="index.php?action=administration&want=editComment&comment_id=<?= $data['id']; ?>" title="Editer le commentaire" id="edit-comment-btn"><i class="fas fa-edit"></i></a>
 		<a href="index.php?action=administration&want=confirmComment&comment_id=<?= $data['id']; ?>" title="Confirmer le commentaire" id="confirm-comment-btn"><i class="fas fa-check-circle"></i></a>
-	</div>
-	<div id="comment-date-contenair">
 		<p><?= $data["creation_date_fr"]; ?></p>
 	</div>
-	<div id="comment-content-contenair">
-		<p><?= $data["content"]; ?></p>
-	</div>
+	<p id="p-comment-content"><?= $data["content"]; ?></p>
 </div>
 <script>
 	document.querySelector("#delete-comment-btn").addEventListener("click", function(e){
