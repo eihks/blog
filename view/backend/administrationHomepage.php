9db8ecec421a9bc53780bf29ce4c4d2afec5ce59
Vincent <vincetonin2007@hotmail.fr>
2018-02-10 20:42:08 +0100

    Ajout d'une id au h1

--- view/backend/administrationHomepage.php
+++ view/backend/administrationHomepage.php
@@ -3,7 +3,7 @@ $title="Administration";
 ob_start();
 ?>
 
-<h1>Bienvenue sur la page d'administration</h1>
+<h1 id="h1-homepage">Bienvenue sur la page d'administration</h1>
 <?php
 $content = ob_get_clean();
 require("view/backend/template.php");
