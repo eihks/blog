00f4b0755acb19fd2de7821e9d74dacc1de694b8
Vincent <vincetonin2007@hotmail.fr>
2018-02-13 15:06:50 +0100

    Ajout d'un span pour contenir les icones edit et delete pour pouvoir utiliser flexbox

--- view/backend/seePostsView.php
+++ view/backend/seePostsView.php
@@ -19,7 +19,7 @@ ob_start();
 				$color = "#eef5fc";
 			}
 		?>
-		<li style="background-color : <?= $color; ?>"><a href="index.php?action=administration&want=editPost&id_post=<?= $datas['id']; ?>" title="Editer le ticket"><?= $datas["title"]; ?></a><a href="index.php?action=administration&want=editPost&id_post=<?= $datas['id']; ?>" title="Editer le ticket"><i class="fas fa-edit"></i></a><a href="index.php?action=administration&want=deletePost&id_post=<?= $datas['id']; ?>" title="Supprimer le ticket"><i class="fas fa-trash-alt delete-btn"></i></a></li>
+		<li style="background-color : <?= $color; ?>"><a href="index.php?action=administration&want=editPost&id_post=<?= $datas['id']; ?>" title="Editer le ticket"><?= $datas["title"]; ?></a><span><a href="index.php?action=administration&want=editPost&id_post=<?= $datas['id']; ?>" title="Editer le ticket"><i class="fas fa-edit"></i></a><a href="index.php?action=administration&want=deletePost&id_post=<?= $datas['id']; ?>" title="Supprimer le ticket"><i class="fas fa-trash-alt delete-btn"></i></a></span></li>
 		<?php 
 		}
 		?>
