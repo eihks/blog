528e61c9180b351f00fbb10c606a5d24440b034f
Vincent <vincetonin2007@hotmail.fr>
2018-02-15 16:02:26 +0100

    Ajout d'un label pour les input title ainsi qu'une id pour le css, ajout d'une class mceEditor sur le textarea permettant de créer un nouveau post

--- view/backend/editPostView.php
+++ view/backend/editPostView.php
@@ -7,7 +7,7 @@ $data = $post->fetch();
 ?>
 <div id="edit-post">
 	<form action="index.php?action=administration&want=editPost&id_post= <?= $data['id']; ?>" method="POST">
-		<input type="text" name="title" value="<?= $data['title'] ?>"><br>
+		<label>Titre du ticket<br><input id="input-title" type="text" name="title" value="<?= $data['title'] ?>"></label><br>
 		<textarea id="textarea-edit-post" name="content" class="mceEditor"><?= nl2br($data["content"]); ?></textarea>
 		<button type="submit" id="btn-update-ticket">Mettre à jour</button>
 		<button type="button" id="btn-stop-update">Annuler</button>
@@ -19,8 +19,8 @@ else
 {
 ?>
 <form action="index.php?action=administration&want=newPost" method="POST">
-	<input type="text" name="title"><br>
-	<textarea id="textarea-edit-post" name="content"></textarea>
+	<label>Titre du ticket<br><input id="input-title" type="text" name="title"></label><br>
+	<textarea id="textarea-edit-post" class="mceEditor" name="content"></textarea>
 	<button type="submit" id="btn-update-ticket">Mettre en ligne</button>
 	<button type="button" id="btn-stop-update">Annuler</button>
 <?php
