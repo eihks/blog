df14644efc9deacc52746b85ae7dda36b88372c8
Vincent <vincetonin2007@hotmail.fr>
2018-02-15 16:04:38 +0100

    Ajout d'une class sur les label pour les inputs title

--- view/backend/editPostView.php
+++ view/backend/editPostView.php
@@ -7,7 +7,7 @@ $data = $post->fetch();
 ?>
 <div id="edit-post">
 	<form action="index.php?action=administration&want=editPost&id_post= <?= $data['id']; ?>" method="POST">
-		<label>Titre du ticket<br><input id="input-title" type="text" name="title" value="<?= $data['title'] ?>"></label><br>
+		<label class="label-title-input">Titre du ticket<br><input id="input-title" type="text" name="title" value="<?= $data['title'] ?>"></label><br>
 		<textarea id="textarea-edit-post" name="content" class="mceEditor"><?= nl2br($data["content"]); ?></textarea>
 		<button type="submit" id="btn-update-ticket">Mettre à jour</button>
 		<button type="button" id="btn-stop-update">Annuler</button>
@@ -19,7 +19,7 @@ else
 {
 ?>
 <form action="index.php?action=administration&want=newPost" method="POST">
-	<label>Titre du ticket<br><input id="input-title" type="text" name="title"></label><br>
+	<label class="label-title-input">Titre du ticket<br><input id="input-title" type="text" name="title"></label><br>
 	<textarea id="textarea-edit-post" class="mceEditor" name="content"></textarea>
 	<button type="submit" id="btn-update-ticket">Mettre en ligne</button>
 	<button type="button" id="btn-stop-update">Annuler</button>
