f276ce3108f7fdafe4e4567789e294b5acf261c0
Vincent <vincetonin2007@hotmail.fr>
2018-02-15 16:14:01 +0100

    Ajout de lien sur les boutons 'Annuler' présent audessous des textarea permettant de revenir sur la liste des tickets, et ajout d'un script JS demandant une confirmation lors d'un click sur ce bouton

--- view/backend/editPostView.php
+++ view/backend/editPostView.php
@@ -10,7 +10,7 @@ $data = $post->fetch();
 		<label class="label-title-input">Titre du ticket<br><input id="input-title" type="text" name="title" value="<?= $data['title'] ?>"></label><br>
 		<textarea id="textarea-edit-post" name="content" class="mceEditor"><?= nl2br($data["content"]); ?></textarea>
 		<button type="submit" id="btn-update-ticket">Mettre à jour</button>
-		<button type="button" id="btn-stop-update">Annuler</button>
+		<a href="index.php?action=administration&want=seePosts"><button type="button" id="btn-cancel">Annuler</button></a>
 	</form>
 </div>
 <?php
@@ -22,9 +22,21 @@ else
 	<label class="label-title-input">Titre du ticket<br><input id="input-title" type="text" name="title"></label><br>
 	<textarea id="textarea-edit-post" class="mceEditor" name="content"></textarea>
 	<button type="submit" id="btn-update-ticket">Mettre en ligne</button>
-	<button type="button" id="btn-stop-update">Annuler</button>
+	<a href="index.php?action=administration&want=seePosts"><button type="button" id="btn-cancel">Annuler</button></a>
 <?php
 }
 $content = ob_get_clean();
 require("view/backend/template.php");
 ?>
+<script type="text/javascript">
+	var btnsCancel = document.querySelectorAll("#btn-cancel");
+	for(var i=0; i < btnsCancel.length; i++)
+	{
+		btnsCancel[i].addEventListener("click", function(e){
+			if(confirm("Voulez-vous vraiment annuler l'édition ?") == false)
+			{
+				e.preventDefault();
+			}
+		})
+	}
+</script>
