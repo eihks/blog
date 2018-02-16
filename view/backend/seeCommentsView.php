47c005fb8423623b76421cb5f1062cdd749d4621
Vincent <vincetonin2007@hotmail.fr>
2018-02-16 21:30:25 +0100

    Ajout de classe sur les différents icones d'adminstration

--- view/backend/seeCommentsView.php
+++ view/backend/seeCommentsView.php
@@ -22,9 +22,9 @@ ob_start();
 				<a href="index.php?action=postpage&id_post=<?= $datas['id_post']; ?>">Commentaire N°<?= $i ?></a>
 				<p class="p-Nreport">Signalements : <?= $datas["report_level"] ?></p>
 				<span>
-					<a href="index.php?action=administration&want=deleteComment&comment_id=<?= $datas['id']; ?>" title="Supprimer le commentaire" id="delete-comment-btn"><i class="fas fa-trash-alt delete-btn"></i></a>
-					<a href="index.php?action=administration&want=editComment&comment_id=<?= $datas['id']; ?>" title="Editer le commentaire" id="edit-comment-btn"><i class="fas fa-edit"></i></a>
-					<a href="index.php?action=administration&want=confirmComment&comment_id=<?= $datas['id']; ?>" title="Valider le commentaire" id="confirm-comment-btn"><i class="fas fa-check-circle"></i></a>
+					<a href="index.php?action=administration&want=deleteComment&comment_id=<?= $datas['id']; ?>" title="Supprimer le commentaire" id="delete-comment-btn"><i class="fas fa-trash-alt delete-ico"></i></a>
+					<a href="index.php?action=administration&want=editComment&comment_id=<?= $datas['id']; ?>" title="Editer le commentaire" id="edit-comment-btn"><i class="fas fa-edit edit-ico"></i></a>
+					<a href="index.php?action=administration&want=confirmComment&comment_id=<?= $datas['id']; ?>" title="Valider le commentaire" id="confirm-comment-btn"><i class="fas fa-check-circle confirm-ico"></i></a>
 				</span>
 			</li>
 		<?php
