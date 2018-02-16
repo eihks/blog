ba210f9bfd33f59fda1df9ed9be902ae491f2a0a
Vincent <vincetonin2007@hotmail.fr>
2018-02-16 21:19:18 +0100

    Ajout d'un script JS pour confirmer l'edition d'un commentaireà l'aide d'un confirm, suppression de la class mceEditor sur le textarea

--- view/backend/editCommentView.php
+++ view/backend/editCommentView.php
@@ -6,7 +6,7 @@ $data = $comment->fetch();
 
 <div id="edit-comment">
 	<form action="index.php?action=administration&want=editComment&comment_id= <?= $data['id']; ?>" method="POST">
-		<textarea id="textarea-edit-comment" class="mceEditor" name="content"><?= $data["content"]; ?></textarea>
+		<textarea id="textarea-edit-comment" name="content"><?= $data["content"]; ?></textarea>
 		<button type="submit" class="btn-update">Mettre à jour</button>
 		<a href="index.php?action=administration&want=seeComments"><button type="button" class="btn-cancel">Annuler</button></a>
 	</form>
@@ -25,4 +25,11 @@ require("view/backend/template.php");
 				e.preventDefault();
 			}
 		})
+
+	document.querySelector(".btn-update").addEventListener("click", function(e){
+		if(confirm("Voulez-vous vraiment confirmer la modification de ce commentaire ? Après validation ce commentaire n'apparaitra plus comme un commentaire signalé. \nContinuer ?") == false)
+		{
+			e.preventDefault();
+		}
+	})
 </script>
