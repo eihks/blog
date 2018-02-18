a1ea466b1793d81f1cdc97fa45f6b7e1ad616d35
Vincent <vincetonin2007@hotmail.fr>
2018-02-18 14:41:08 +0100

    Ajout d'un bouton permettant l'ajout d'un ticket

--- view/backend/administrationHomepage.php
+++ view/backend/administrationHomepage.php
@@ -4,6 +4,7 @@ ob_start();
 ?>
 
 <h1 id="h1-homepage">Bienvenue sur la page d'administration</h1>
+<a href="index.php?action=administration&want=newPost"><button type="button" id="btn-new-post">Nouveau Ticket</button></a>
 <?php
 $content = ob_get_clean();
 require("view/backend/template.php");
