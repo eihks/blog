ef4bde6e521ee3cc1c77c86f4fef4c556ebcd951
Vincent <vincetonin2007@hotmail.fr>
2018-02-18 18:10:43 +0100

    Ajout de div, et de variable dataP et dataC contenant le dernier commentaire et le dernier ticket, puis affichage de ses derniers dans leurs div respective

--- view/backend/administrationHomepage.php
+++ view/backend/administrationHomepage.php
@@ -1,10 +1,28 @@
 <?php 
 $title="Administration";
 ob_start();
+$dataP = $post->fetch();
+$dataC = $comment->fetch();
 ?>
 
 <h1 id="h1-homepage">Bienvenue sur la page d'administration</h1>
 <a href="index.php?action=administration&want=newPost"><button type="button" id="btn-new-post">Nouveau Ticket</button></a>
+<div id="last-post">
+	<div class="ticket">
+		<a class="a-ticket" href="index.php?action=postpage&title=<?= $dataP['title'] ?>&id_post=<?= $dataP['id']; ?> ">
+			<h2>Dernier ticket</h2>
+			<span class="title"><h2><?= $dataP["title"]; ?></h2></span>
+			<span class="text"><p><?= $dataP["content"]; ?></p></span>
+			<span class="author"><p><?= $dataP["author"]; ?></p></span>
+			<span class ="date"><p><?= $dataP["creation_date_fr"]; ?></p></span>
+		</a>
+	</div>
+</div>
+
+<div id="last-comment">
+	<h2>Dernier Commentaire</h2>
+	<p><?= $dataC["content"]; ?></p>
+</div>
 <?php
 $content = ob_get_clean();
 require("view/backend/template.php");
