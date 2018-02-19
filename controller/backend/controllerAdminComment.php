6bb9cc33e727f4d9e11d4d1f7c72ff41e07f451d
Vincent <vincetonin2007@hotmail.fr>
2018-02-19 11:28:41 +0100

    Contient tout les appelles lié aux commentaires pour l'administration

---
+++ controller/backend/controllerAdminComment.php
@@ -0,0 +1,83 @@
+<?php
+require_once("model/CommentManager.php");
+require_once("model/PostManager.php");
+class ControllerAdminComment{
+	public static function seeComments()
+	{
+		if(isset($_SESSION["isLog"]) AND $_SESSION["isLog"] == true)
+		{
+			$commentManager = new CommentManager();
+			$comments = $commentManager->getReportedComments();
+			require("view/backend/seeCommentsView.php");
+		}
+		else
+		{
+			throw new Exception("Vous n'étes pas connecter ! ");
+		}	
+	}
+
+	public static function controlComment()
+	{
+		if(isset($_SESSION["isLog"]) AND $_SESSION["isLog"] == true)
+		{
+			$commentManager = new CommentManager();
+			$comment = $commentManager->getComment();
+			require("view/backend/seeCommentView.php");
+		}
+		else
+		{
+			throw new Exception("Vous n'étes pas connecter ! ");
+		}	
+	}
+
+	public static function deleteComment()
+	{
+		if(isset($_SESSION["isLog"]) AND $_SESSION["isLog"] == true)
+		{
+			$commentManager = new CommentManager();
+			$comment = $commentManager->deleteComment();
+			header("Location: index.php?action=administration&want=seeComments");
+		}
+		else
+		{
+			throw new Exception("Vous n'étes pas connecter ! ");
+		}		
+	}
+
+	public static function editComment()
+	{
+		if(isset($_SESSION["isLog"]) AND $_SESSION["isLog"] == true)
+		{
+			if(isset($_POST["content"]))
+			{
+				$commentManager = new CommentManager();
+				$comment = $commentManager->editComment();
+				header("Location: index.php?action=administration&want=seeComments");
+			}
+			else
+			{
+				$commentManager = new CommentManager();
+				$comment = $commentManager->getComment();
+				require("view/backend/editCommentView.php");
+			}
+		}
+		else
+		{
+			throw new Exception("Vous n'étes pas connecter ! ");
+		}	
+	}
+
+	public static function confirmComment()
+	{
+		if(isset($_SESSION["isLog"]) AND $_SESSION["isLog"] == true)
+		{
+			$commentManager = new CommentManager();
+			$comment = $commentManager->confirmComment();
+			header("Location: index.php?action=administration&want=seeComments");
+		}
+		else
+		{
+			throw new Exception("Vous n'étes pas connecter ! ");
+		}
+	}
+}
