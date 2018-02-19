6703bc10ea32023eb063edfcbb6d683ff09fddca
Vincent <vincetonin2007@hotmail.fr>
2018-02-19 13:00:58 +0100

    Changement du chemin vers le fichier css

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -2,9 +2,10 @@
 	<html lang="fr">
 		<head>
 			<meta charset="utf-8" />
-			<link rel="stylesheet" href="public/<?= $cssFile; ?>" />
-			<link rel="stylesheet" href="public/fontawesome-free-5.0.6/web-fonts-with-css/css/fontawesome-all">
-			<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+			<link rel="stylesheet" href="public/front-css/<?= $cssFile; ?>" />
+			<link rel="stylesheet" href="public/fontawesome-free-5.0.6/web-fonts-with-css/css/fontawesome-all" />
+			<link rel="stylesheet" href="public/front-css/styleTemplateFront.css" />
+			<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
 			<title> <?= $title ?></title>
 		</head>
 
