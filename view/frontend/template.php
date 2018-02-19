df3e5c2e33312090bcff659f8a872a4b030b61d2
Vincent <vincetonin2007@hotmail.fr>
2018-02-19 12:43:11 +0100

    Ajout d'une div contenant un avertissement sur les cookies, et d'un script JS permettant d'afficher ou non la div si l'utilisation des cookies a était accepté

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -9,6 +9,26 @@
 		</head>
 
 		<body>
+			<div id="cookie-advertissement">
+				<p>Les cookies nous permettent de vérifier certaines informations lié au site et de facilister le partage de données entre les différentes pages,
+				 	en poursuivant votre navigation vous acceptez leurs utilisations. 
+					<a href="https://support.google.com/accounts/answer/61416?co=GENIE.Platform%3DDesktop&hl=fr">Plus d'informations</a>
+					<button id="accept-cookie-btn">Accepter</button>
+				</p>
+			</div>
 			<?= $content; ?>
+			<script type="text/javascript">
+				if(localStorage.getItem("acceptCookie") === "true")
+				{
+					document.querySelector("#cookie-advertissement").style.display="none";
+				}
+				else
+				{
+					document.querySelector("#accept-cookie-btn").addEventListener("click", function(){
+						document.querySelector("#cookie-advertissement").style.display ="none";
+						localStorage.setItem("acceptCookie", true);
+					})
+				}
+			</script>
 		</body>
 	</html>
