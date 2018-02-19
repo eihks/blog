513a5bb8c06d4d8ffaf47662cf901d5d38fcb505
Vincent <vincetonin2007@hotmail.fr>
2018-02-19 11:29:18 +0100

    Contient tout les appelles lié aux poste pour l'administration

---
+++ controller/backend/controllerAdminPost.php
@@ -0,0 +1,118 @@
+<?php
+require_once("model/CommentManager.php");
+require_once("model/PostManager.php");
+class ControllerAdminPost{
+	public static function login()
+	{
+		if(isset($_SESSION["isLog"]) AND $_SESSION["isLog"] == true)
+		{
+			$postManager = new PostManager();
+			$post = $postManager->getLastPost();
+			$commentManager = new CommentManager();
+			$comment = $commentManager->getLastComment();
+			require("view/backend/administrationHomepage.php");
+		}
+		else
+		{
+			$_SESSION["isLog"] = false;
+			$passwordHash = password_hash("test", PASSWORD_DEFAULT);
+			if(isset($_POST["password"]) AND password_verify($_POST["password"], $passwordHash))
+			{
+				$_SESSION["isLog"] = true;
+				$postManager = new PostManger();
+				$post = $postManager->getLastPost();
+				$commentManager = new CommentManager();
+				$comment = $commentManager->getLastComment();
+				require("view/backend/administrationHomepage.php");
+			}
+			else
+			{
+				echo "pas ok";
+				echo $passwordHash;
+				require("view/backend/loginView.php");
+			}
+		}
+	}
+
+	public static function seePosts()
+	{
+		if(isset($_SESSION["isLog"]) AND $_SESSION["isLog"] == true)
+		{
+			$postManager = new PostManager();
+			$posts = $postManager->getPosts();
+			require("view/backend/seePostsView.php");
+		}
+		else
+		{
+			require("view/backend/loginView.php");
+		}
+	}
+
+	public static function editPost()
+	{
+		if(isset($_SESSION["isLog"]) AND $_SESSION["isLog"] == true)
+		{
+			$postManager = new PostManager();
+			$post = $postManager->getPost();
+			require("view/backend/editPostView.php");
+		}
+		else
+		{
+			require("view/backend/loginView.php");
+		}
+	}
+
+	public static function updatePost()
+	{
+		if(isset($_SESSION["isLog"]) AND $_SESSION["isLog"] == true)
+		{
+			$postManager = new PostManager();
+			$post = $postManager->updatePost($_POST["title"], $_POST["content"], $_GET["id_post"]);
+			header("Location: index.php?action=administration");
+		}
+		else
+		{
+			throw new Exception("Vous n'étes pas connecter ! ");
+		}
+	}
+
+	public static function newPost()
+	{
+		if(isset($_SESSION["isLog"]) AND $_SESSION["isLog"] == true)
+		{
+			require("view/backend/editPostView.php");
+		}
+		else
+		{
+			throw new Exception("Vous n'étes pas connecter ! ");
+		}
+	}
+
+	public static function insertPost()
+	{
+		if(isset($_SESSION["isLog"]) AND $_SESSION["isLog"] == true)
+		{
+			$postManager = new PostManager();
+			$post = $postManager->insertPost($_POST["title"], $_POST["content"]);
+			header("Location: index.php?action=administration");
+		}
+		else
+		{
+			throw new Exception("Vous n'étes pas connecter ! ");
+		}	
+	}
+
+	public static function deletePost()
+	{
+		if(isset($_SESSION["isLog"]) AND $_SESSION["isLog"] == true)
+		{
+			$postManager = new PostManager();
+			$post = $postManager->deletePost();
+			header("Location: index.php?action=administration");
+		}
+		else
+		{
+			throw new Exception("Vous n'étes pas connecter ! ");
+		}	
+	}
+}
