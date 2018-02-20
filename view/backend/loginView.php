8fff8868be43ebcb7a104a0898f9dfa160f379a5
Vincent <vincetonin2007@hotmail.fr>
2018-02-20 20:10:00 +0100

    suppression de la variable cssFile (pas de fichier css pour cette page pour le moment)

--- view/backend/loginView.php
+++ view/backend/loginView.php
@@ -1,10 +1,9 @@
 <?php 
 $title ="Administration";
-$cssFile = "";
 ob_start();
 ?>
 <form action="index.php?action=administration" method="POST">
-	<input type="password" name="password">
+	<label>Mot de passe :<input type="password" name="password"></label>
 </form>
 <?php
 $content = ob_get_clean();
