87872d730722468d7635ea492d620c52b1e9dfd8
Vincent <vincetonin2007@hotmail.fr>
2018-02-22 15:54:58 +0100

    Modification de la premiere methode du controller (anciennement seeComments, ce nomme desormais seeAllComments) pour appeller la bonne methode, creation d'une nouvelle methode seeReportedComments pour recuperer les commentaires signaler et appeller la bonne vue

--- controller/backend/controllerAdminComment.php
+++ controller/backend/controllerAdminComment.php
@@ -2,18 +2,25 @@
 require_once("model/CommentManager.php");
 require_once("model/PostManager.php");
 class ControllerAdminComment{
-	public static function seeComments()
+	public static function seeAllComments()
 	{
 		if(isset($_SESSION["isLog"]) AND $_SESSION["isLog"] == true)
 		{
 			$commentManager = new CommentManager();
-			$comments = $commentManager->getReportedComments();
+			$comments = $commentManager->getAllComments();
 			require("view/backend/seeCommentsView.php");
 		}
 		else
 		{
 			throw new Exception("Vous n'étes pas connecter ! ");
 		}	
+	}
+
+	public static function seeReportedComments()
+	{
+		$commentManager = new CommentManager();
+		$comments = $commentManager->getReportedComments();
+		require("view/backend/seeReportedCommentsView.php");
 	}
 
 	public static function controlComment()
