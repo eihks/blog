ca027d952ace021c2a0b2ed27d3ec68de4427d7e
Vincent <vincetonin2007@hotmail.fr>
2018-02-22 15:56:16 +0100

    Affiche desormais tout les commentaires, signaler ou non (c/c de l'ancienne page suppression uniquement de la ligne affichant le nombre de signalement du commentaire)

--- view/backend/seeCommentsView.php
+++ view/backend/seeCommentsView.php
@@ -21,7 +21,6 @@ ob_start();
 		?>
 			<li style="background-color : <?= $color; ?>">
 				<a href="index.php?action=postpage&id_post=<?= $datas['id_post']; ?>">Commentaire N°<?= $i ?></a>
-				<p class="p-Nreport">Signalements : <?= $datas["report_level"] ?></p>
 				<span>
 					<a href="index.php?action=administration&want=deleteComment&comment_id=<?= $datas['id']; ?>" title="Supprimer le commentaire" id="delete-comment-btn"><i class="fas fa-trash-alt delete-ico"></i></a>
 					<a href="index.php?action=administration&want=editComment&comment_id=<?= $datas['id']; ?>" title="Editer le commentaire" id="edit-comment-btn"><i class="fas fa-edit edit-ico"></i></a>
