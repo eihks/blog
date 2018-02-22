9a87237a8887e649c7e65eb67e9670e6e77dac48
Vincent <vincetonin2007@hotmail.fr>
2018-02-22 20:33:54 +0100

    Suppression du bloc if verifiant si l'utilisateur est connecter pour acceder aux controller (verifier dans le rooter désormais), passage des variables comme parametre lors d'apelles des differentes methodes

--- controller/backend/controllerAdminPost.php
+++ controller/backend/controllerAdminPost.php
@@ -2,115 +2,43 @@
 require_once("model/CommentManager.php");
 require_once("model/PostManager.php");
 class ControllerAdminPost{
-	public static function login()
-	{
-		if(isset($_SESSION["isLog"]) AND $_SESSION["isLog"] == true)
-		{
-			$postManager = new PostManager();
-			$post = $postManager->getLastPost();
-			$commentManager = new CommentManager();
-			$comments = $commentManager->get5LastsComment();
-			require("view/backend/administrationHomepage.php");
-		}
-		else
-		{
-			$_SESSION["isLog"] = false;
-			$passwordHash = password_hash("test", PASSWORD_DEFAULT);
-			if(isset($_POST["password"]) AND password_verify($_POST["password"], $passwordHash))
-			{
-				$_SESSION["isLog"] = true;
-				$postManager = new PostManager();
-				$post = $postManager->getLastPost();
-				$commentManager = new CommentManager();
-				$comments = $commentManager->get5LastsComment();
-				require("view/backend/administrationHomepage.php");
-			}
-			else
-			{
-				require("view/backend/loginView.php");
-			}
-		}
-	}
-
 	public static function seePosts()
 	{
-		if(isset($_SESSION["isLog"]) AND $_SESSION["isLog"] == true)
-		{
-			$postManager = new PostManager();
-			$posts = $postManager->getPosts();
-			require("view/backend/seePostsView.php");
-		}
-		else
-		{
-			require("view/backend/loginView.php");
-		}
+		$postManager = new PostManager();
+		$posts = $postManager->getPosts();
+		require("view/backend/seePostsView.php");
 	}
 
 	public static function editPost()
 	{
-		if(isset($_SESSION["isLog"]) AND $_SESSION["isLog"] == true)
-		{
-			$postManager = new PostManager();
-			$post = $postManager->getPost();
-			require("view/backend/editPostView.php");
-		}
-		else
-		{
-			require("view/backend/loginView.php");
-		}
+		$postManager = new PostManager();
+		$post = $postManager->getPost($_GET["id_post"]);
+		require("view/backend/editPostView.php");
 	}
 
 	public static function updatePost()
 	{
-		if(isset($_SESSION["isLog"]) AND $_SESSION["isLog"] == true)
-		{
-			$postManager = new PostManager();
-			$post = $postManager->updatePost($_POST["title"], $_POST["content"], $_GET["id_post"]);
-			header("Location: index.php?action=administration");
-		}
-		else
-		{
-			throw new Exception("Vous n'étes pas connecter ! ");
-		}
+		$postManager = new PostManager();
+		$post = $postManager->updatePost($_POST["title"], $_POST["content"], $_GET["id_post"]);
+		header("Location: index.php?action=administration");
 	}
 
 	public static function newPost()
 	{
-		if(isset($_SESSION["isLog"]) AND $_SESSION["isLog"] == true)
-		{
-			require("view/backend/editPostView.php");
-		}
-		else
-		{
-			throw new Exception("Vous n'étes pas connecter ! ");
-		}
+		require("view/backend/editPostView.php");
 	}
 
 	public static function insertPost()
 	{
-		if(isset($_SESSION["isLog"]) AND $_SESSION["isLog"] == true)
-		{
-			$postManager = new PostManager();
-			$post = $postManager->insertPost($_POST["title"], $_POST["content"]);
-			header("Location: index.php?action=administration");
-		}
-		else
-		{
-			throw new Exception("Vous n'étes pas connecter ! ");
-		}	
+		$postManager = new PostManager();
+		$post = $postManager->insertPost($_POST["title"], $_POST["content"]);
+		header("Location: index.php?action=administration");	
 	}
 
 	public static function deletePost()
 	{
-		if(isset($_SESSION["isLog"]) AND $_SESSION["isLog"] == true)
-		{
-			$postManager = new PostManager();
-			$post = $postManager->deletePost();
-			header("Location: index.php?action=administration");
-		}
-		else
-		{
-			throw new Exception("Vous n'étes pas connecter ! ");
-		}	
+		$postManager = new PostManager();
+		$post = $postManager->deletePost($_GET["id_post"]);
+		header("Location: index.php?action=administration");
 	}
 }
