f3a6e0dae3eee8fadc469bb1987014a624f5171f
Vincent <vincetonin2007@hotmail.fr>
2018-02-22 20:28:50 +0100

    Controller contenant désormais la méthode login

---
+++ controller/backend/controllerAdmin.php
@@ -0,0 +1,35 @@
+<?php
+ini_set("display_errors",0);error_reporting(0);
+require_once("model/CommentManager.php");
+require_once("model/PostManager.php");
+class ControllerAdmin{
+	public static function login()
+	{
+		if(isset($_SESSION["isLog"]) AND $_SESSION["isLog"] == true)
+		{
+			$postManager = new PostManager();
+			$post = $postManager->getLastPost();
+			$commentManager = new CommentManager();
+			$comments = $commentManager->get5LastsComment();
+			require("view/backend/administrationHomepage.php");
+		}
+		else
+		{
+			$_SESSION["isLog"] = false;
+			$passwordHash = password_hash("test", PASSWORD_DEFAULT);
+			if(isset($_POST["password"]) AND password_verify($_POST["password"], $passwordHash))
+			{
+				$_SESSION["isLog"] = true;
+				$postManager = new PostManager();
+				$post = $postManager->getLastPost();
+				$commentManager = new CommentManager();
+				$comments = $commentManager->get5LastsComment();
+				require("view/backend/administrationHomepage.php");
+			}
+			else
+			{
+				require("view/backend/loginView.php");
+			}
+		}
+	}
+}
