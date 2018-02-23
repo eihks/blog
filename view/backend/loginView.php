ace131df2ea42dba3d78129ab4764746eb86e97d
Vincent <vincetonin2007@hotmail.fr>
2018-02-23 14:22:04 +0100

    Ajout d'un titre et d'un lien vers la homepage du blog

--- view/backend/loginView.php
+++ view/backend/loginView.php
@@ -1,10 +1,13 @@
 <?php 
 $title ="Administration";
+$cssFile = "styleLogin.css";
 ob_start();
 ?>
+<h1>Accéder à l'administration</h1>
 <form action="index.php?action=administration" method="POST">
 	<label>Mot de passe :<input type="password" name="password"></label>
 </form>
+<a href="index.php?action=homepage&page=1">Retourner sur la page d'accuei du blog</a>
 <?php
 $content = ob_get_clean();
 require("view/frontend/template.php");
