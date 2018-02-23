d4dcc1896c93074f4c1d470454e706626ab99fd2
Vincent <vincetonin2007@hotmail.fr>
2018-02-23 13:11:51 +0100

    Ajout d'une methode logOut

--- controller/backend/controllerAdmin.php
+++ controller/backend/controllerAdmin.php
@@ -31,4 +31,17 @@ class ControllerAdmin{
 			}
 		}
 	}
+
+	public static function logOut()
+	{
+		if (ini_get("session.use_cookies")) {
+		    $params = session_get_cookie_params();
+		    setcookie(session_name(), '', time() - 42000,
+		        $params["path"], $params["domain"],
+		        $params["secure"], $params["httponly"]
+		    );
+		}
+		session_destroy();
+		header("Location: index.php?action=homepage&page=1");
+	}
 }
